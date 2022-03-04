60ef2c1f94ca47f7603acb9db3e06dbb2a23be6c
lstubdal <80265502+lstubdal@users.noreply.github.com>
2022-03-04 10:36:04 +0100

    Hide dealers first card

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="table">
-        <div class="table__game-finished" v-if="!gameFinished">
-            <h2>STATUS OF GAME </h2>
+    <div class="table"> <!-- semantikk -->
+        <div class="table__game-finished" v-if="gameFinished">
             <RouterLink :to="{ name: 'home'}">
                 <button>PLAY AGAIN</button>
             </RouterLink>
@@ -12,12 +11,14 @@
         </RouterLink>
 
         <h2 class="table__participants table__participants--dealer">{{ participants[0] }} 
-            <div class="table__sum">Sum: {{ dealersTotalSum }}</div>
+            <div v-if="!playerDone" class="table__sum">Sum: {{ dealersTotalSum }}</div>       <!-- show dealers sum if player hit stay -->
         </h2>
 
         <div class="dealer">
-            <div v-for="card in dealersCards">
-                <img :src="card.image" :alt="card.code" class="table__card">
+            <div v-if="playerDone" class="dealer__first-card">dealers hidden card</div>
+            <div class="dealer__cards"  v-for="(card, index) in dealersCards">
+                <img v-if="!playerDone && index === 0" :src="card.image" :alt="card.code" class="dealer__card">
+                <img v-if="index > 0" :src="card.image" :alt="card.code" class="dealer__card">    <!-- kanskje??? :class="`dealer__card + index ${playerDone && index === 0 ? 'dealer__first-card' : ''}`" -->
             </div>
         </div>
 
@@ -32,27 +33,29 @@
             <div class="table__sum">Sum: {{ playersTotalSum }}</div>
         </h2>
 
-        <div class="player">
+        <div class="player">    <!-- section -->
             <div class="player__cards" v-for="card in playersCards">
-                <img :src="card.image" :alt="card.code" class="table__card">
+                <img :src="card.image" :alt="card.code" class="player__card">
             </div>
         </div>
 
         <div class="table__buttons">
-                <button class="table__button">{{ buttonText[0] }}</button>
-                <button class="table__button">{{ buttonText[1] }}</button>
+                <button class="table__button" @click="drawCardPlayer">{{ buttonText[0] }}</button>
+                <button class="table__button" @click="showDealersCard">{{ buttonText[1] }}</button>
         </div>
 
     </div>
 </template>
 
 <script>
+    
     export default {
         data() {
             return {
                 participants: ['DEALER', 'YOU'],
                 buttonText: ['HIT ME', 'STAY'],
-                gameStatus: ['No one has Black Jack', 'Black Jack! Dealer wins', 'Black Jack! You win', 'Dealers turn...'],
+                gameStatusMessage: ['No one has Black Jack', 'Black Jack! Dealer wins', 'Black Jack! You win', "Dealer's busted, you win!", "You're busted, dealer wins", 'Dealer draws a card'],
+                gameStatus: '',
                 deckId: '',
                 dealersCards: [],
                 playersCards: [],
@@ -60,7 +63,9 @@
                 playersSum: 0,
                 remainingCards: '',
                 blackjack: 21,
-                gameFinished: false
+                gameFinished: false,
+                playerDone: true,
+                dealerDrawsCard: false
             }
         },
 
@@ -69,27 +74,62 @@
         },
 
         computed: {
+             dealersDrawsCard() {
+                return this.dealersDrawsCard = !this.dealerDrawsCard
+            },
+
+            dealerFinished() {
+                this.playerDone = !this.playerDone;
+            },
+
             getStatusOfGame() {
                 /* check default table status */
-                if (this.dealersCards.length === 2 && this.playersCards.length === 2) {
-                    if (this.dealersTotalSum != this.blackjack && this.playersTotalSum != this.blackjack){ /* if you have blackjack */
-                        this.gameFinished = true;
-                        return this.gameStatus[0];
+       
+                    if (this.dealersTotalSum != this.blackjack && this.playersTotalSum != this.blackjack){ /* if neither have black jack */
+                        return this.gameStatusMessage[0];
                     }
 
                     if (this.dealersTotalSum === this.blackjack && this.playersTotalSum != this.blackjack){ /* if dealer has blackjack */
+                        this.gameFinished = !this.gameFinished;
+                        this.dealerFinished();
+                        return this.gameStatusMessage[1]
+
+                    } else if (this.dealersTotalSum === this.blackjack && this.playersTotalSum === this.blackjack){  /* if both has blackjack, dealer also wins */
                         this.gameFinished = true;
-                        return this.gameStatus[1]
-                    } else if (this.dealersTotalSum != this.blackjack && this.playersTotalSum === this.blackjack){  /* if both has blackjack, dealer also wins */
-                        this.gameFinished = true;
-                        return this.gameStatus[1]
+                        this.dealerFinished();
+                        return this.gameStatusMessage[1]
                     }
 
-                    return this.gameStatus[2]   /* if neither has */
-                } 
+                    if (this.dealersTotalSum != this.blackjack && this.playersTotalSum === this.blackjack){ /* if player has black jack */
+                        this.dealerFinished();
+                        return this.gameStatusMessage[2];
+                    }
+
+                /* check if dealer or player are busted */
+                    if (this.dealersSum > this.blackjack ) {
+                        this.dealerFinished();
+                        return this. gameStatusMessage[3];
+                       
+                    } else {
+                        if (this.playersSum > this.blackjack ) {
+                            return this.gameStatusMessage[4];
+                        }
+                    }
+                
+
+                /* check who wins when no one has  */
+                if (this.dealersSum >= 17 && this.dealersSum > this.playersSum) {
+                    this.dealerFinished();
+                    return this.gameStatusMessage[1]
+                }
+
+                if (this.dealerDrawsCard) {
+                    return this.gameStatusMessage[6];
+                }
             },
 
-            dealersTotalSum() {
+
+            dealersTotalSum() { /* SHOW AFTER PLAYER STAYS */
                 this.dealersCards.forEach(card => {
                     if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
                         this.dealersSum += 10
@@ -105,6 +145,8 @@
             },
 
             playersTotalSum() {
+                console.log('player sum først', this.playersSum);
+                
                 this.playersCards.forEach(card => {
                     if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
                         this.playersSum += 10;
@@ -131,39 +173,60 @@
                 this.deckId = deck_id;
 
                 /* draw four cards from deck for default table */
-                const drawUrl = `https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=4`;
-                const drawResponse = await fetch(drawUrl);
-                const data = await drawResponse.json();
+                const defaultCardsUrl = `https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=4`;
+                const defaultCardsResponse = await fetch(defaultCardsUrl);
+                const data = await defaultCardsResponse.json();
 
-                console.log('kort igjen', data.remaining);
-                console.log('cards', data.cards);
-
+                /* add two cards to dealer */
                 this.dealersCards.push(data.cards[0]);
                 this.dealersCards.push(data.cards[1]);
-                console.log('DEALERS KORT', this.dealersCards.length);
 
+                /* add two cards to player */
                 this.playersCards.push(data.cards[2]);
                 this.playersCards.push(data.cards[3]);
-                console.log('PLAYER KORT', this.dealersCards.length);
 
                 this.remainingCards = data.remaining;
             },
 
-        /*     dealersTotalSum() {
-                this.dealersCards.forEach(card => {
-                    if (card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING') {
-                        
-                        this.dealersSum += 10
-                    } else if (card.value === 'ACE' && this.dealersCards.length >= 2) {
-                        this.dealersSum += 11
-                    } else {
-                        const valueAsNumer = parseInt(card.value);
-                        this.dealersSum += valueAsNumer;
-                    }
-                })
-                console.log('dealer', this.dealersSum)
-                return this.dealersSum;
-            }, */
+            async drawCardDealer() {
+                
+            /* when player stays, make dealer draw card while sum is under 17 */
+                while (this.dealersTotalSum < 17 && this.playerDone ) {
+                    const drawUrl = `https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`;
+                    const drawResponse =  await fetch(drawUrl);
+                    const cardData = await drawResponse.json();
+                    
+                     setTimeout( () => {
+                        this.dealersCards.push(cardData.cards[0]);
+                        this.remainingCards = cardData.remaining;       /* update */
+                    }, 2000) 
+                    
+                    this.dealersSum = 0;    /* reset sum for each time player draws a card */
+
+                    console.log('etter push ', this.dealersCards.length);
+
+                }        
+            },
+
+            async drawCardPlayer() {
+                const drawUrl = `https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`;
+                const drawResponse = await fetch(drawUrl);
+                const cardData = await drawResponse.json();
+                
+                console.log('før push ', this.playersCards.length);
+                this.playersCards.push(cardData.cards[0]);
+                this.playersSum = 0;    /* reset sum for each time player draws a card */
+                console.log('etter push ', this.playersCards.length);
+
+                this.remainingCards = cardData.remaining;       /* update */
+            },
+
+            showDealersCard() {
+                this.drawCardDealer();
+                this.dealerFinished();
+                
+            },
+
 
             exit() {
                 // reset game
@@ -224,7 +287,7 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        height: 230px;
+        height: 220px;
         width: 160px;
         font-family: var(--main-font);
         font-size: 3.2em;
@@ -244,6 +307,12 @@
 
     .dealer, .player {
         display: flex;
+        justify-content: center;
+    }
+
+    .dealer__cards {
+        display: flex;
+        justify-content: center;
     }
 
     .table__status {
@@ -254,12 +323,28 @@
         padding-bottom: var(--extra-large);
     }
 
-    .table__card {
+    .dealer__card, .player__card {
         padding: var(--small);
         height: 63%;
     }
 
-    .table__Sum {
+    .dealer__first-card{       /* cover dealers first card until player hits 'stay' */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 140px;
+        font-family: var(--second-font);
+        border-radius: 8px;
+        padding: var(--medium);
+        font-size: 2em;
+        height: 190px;
+        color: var(--light);
+        background-color: var(--dark);
+        margin-top: 10px;
+        border: 2px solid var(--light);
+    }
+
+    .table__sum {
         position: absolute;
         font-family: var(--second-font);
         font-size: 0.5em;
@@ -271,6 +356,25 @@
         bottom: 40;
         margin-top: var(--top-large);
     }
+
+    .table__button {
+        width: 225px;
+        height: 60px;
+        padding: var(--small);
+        font-size: 1.7em;
+        font-family: var(--main-font);
+        cursor: pointer;
+        border-radius: var(--corner-radius);
+        background-color: var(--main-color);
+        border: none;
+        margin: 0px var(--small);
+    }
+    
+    .table__button:hover {
+        color: var(--light);
+        background-color: var(--dark);
+    } 
+</style>
 
     .table__button {
         width: 225px;
