7f7aedac752969191a29a1cb8becd1902718f365
lstubdal <lotte.stubdal@gmail.com>
2022-03-05 10:37:23 +0100

    Remove unused pages

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,7 @@
 import TableView from './views/TableView.vue';
 import HomeView from './views/HomeView.vue';
 
-/* import SinglePlayerView from './views/SinglePlayerView.vue';
-import MultiplePlayerView from './views/MultiplePlayerView.vue'; */
-
 export default [
-	{ name: 'home', 				path: '/', 							component: HomeView },
-	{ name: 'tableview', 			path: '/table', 				component: TableView },
-/* 	{ name: 'singlePlayer', 		path: '/single-player', 		component: SinglePlayerView, 		},
-	{ name: 'multiplePlayers', 		path: '/multiple-player', 		component: MultiplePlayerView, 		}, 
-	{ name: 'player', 				path: '/:player_mode', 			component: PlayerView, 		props: true} */
+	{ name: 'home', 				path: '/', 					component: HomeView },
+	{ name: 'tableview', 			path: '/table', 			component: TableView },
 ]
